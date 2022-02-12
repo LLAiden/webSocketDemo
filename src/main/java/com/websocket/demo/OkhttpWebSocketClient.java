2c47fb734d0f7cf4ebebe25354fd620af0d4a4df
LLAiden <LLAiden@qq.com>
2022-02-12 13:20:57 +0800

    添加okhttp版客户端

--- src/main/java/com/websocket/demo/OkhttpWebSocketClient.java
+++ src/main/java/com/websocket/demo/OkhttpWebSocketClient.java
@@ -1,2 +1,53 @@
-package com.websocket.demo;public class OkhttpWebSocketClient {
+package com.websocket.demo;
+
+import okhttp3.*;
+import okio.ByteString;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class OkhttpWebSocketClient {
+
+    public static void main(String[] args) {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+        Request request = new Request.Builder()
+                .url("ws://localhost:80")
+                .get()
+                .build();
+        WebSocket webSocket = okHttpClient.newWebSocket(request, new WebSocketListener() {
+
+            @Override
+            public void onFailure(@NotNull WebSocket webSocket, @NotNull Throwable t, @Nullable Response response) {
+                t.printStackTrace();
+            }
+
+            @Override
+            public void onMessage(@NotNull WebSocket webSocket, @NotNull String text) {
+                System.out.println("received server message: " + text);
+            }
+
+            @Override
+            public void onMessage(@NotNull WebSocket webSocket, @NotNull ByteString bytes) {
+                String text = bytes.base64();
+                System.out.println("received server message: " + text);
+            }
+
+            @Override
+            public void onOpen(@NotNull WebSocket webSocket, @NotNull Response response) {
+                System.out.println(response);
+            }
+        });
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            int count = 0;
+
+            @Override
+            public void run() {
+                webSocket.send(String.valueOf(count++));
+            }
+        }, 1000, 1000);
+    }
 }
